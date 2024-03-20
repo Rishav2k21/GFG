5efd6e132988821c907811237c643499dd3cc5f6
Rishav Anand <98807848+Rishav2k21@users.noreply.github.com>
2024-03-20 18:30:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of nodes on the longest path from root to leaf node/sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.cpp
@@ -0,0 +1,175 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+structure of the node of the binary tree is as
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+    int maxi=INT_MIN;
+
+    int maxcount=INT_MIN;
+
+void longroot(Node *root, int count, int sum) {
+    if (root == nullptr) {
+        return;
+    }
+    count++;
+    sum += root->data; // Update sum
+
+    if (root->left == nullptr && root->right == nullptr) {
+        if (count > maxcount) {
+            maxcount = count;
+            maxi = sum; // Update maxi with sum when a longer path is found
+        } else if (count == maxcount) {
+            maxi = max(maxi, sum); // Update maxi with sum when path length is the same but sum is greater
+        }
+    }
+
+    longroot(root->left, count, sum);
+    longroot(root->right, count, sum);
+
+    count--;
+}
+  
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        int count=0;
+      
+      longroot(root,count,0);
+      return maxi;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    scanf("%d", &t);
+    cin.ignore();
+    while (t--)
+    {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution obj;
+        int res = obj.sumOfLongRootToLeafPath(root);
+        cout << res << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
